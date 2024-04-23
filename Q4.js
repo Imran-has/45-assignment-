916818b415849a8836de553d659ce5dc17f44a2d
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-04-23 09:25:52 -0700

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,5 @@
+//task#4
+let Quote = "life is like riding a bicycle. To keep your balance,you must keep moving";
+let auther = "albert einstein";
+console.log(`${auther} He said life is like riding a bicycle. To keep your balance,you must keep moving`);
+export {};
---
+++ Q4.ts
@@ -0,0 +1,5 @@
+//task#4
+
+let Quote:string="life is like riding a bicycle. To keep your balance,you must keep moving";
+let auther:string="albert einstein";
+console.log(`${auther} He said life is like riding a bicycle. To keep your balance,you must keep moving`);
