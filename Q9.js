052818e87a2c00006b3d0cb970ac0046e463e755
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-04-27 09:40:53 -0700

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//task#9
+let favouriteResult = 4;
+let messageResult = `My favourite number is : ${favouriteResult}`;
+console.log(messageResult);
---
+++ Q9.ts
@@ -0,0 +1,4 @@
+//task#9
+let favouriteResult:number=4
+let messageResult:string=`My favourite number is : ${favouriteResult}`
+console.log(messageResult)
