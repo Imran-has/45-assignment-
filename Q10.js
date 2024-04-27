6c2906ded0d7f4696fdcb2a05aaa42cb4714bf9b
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-04-27 09:42:10 -0700

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,9 @@
+//task#10
+// this program adds two number and prints the result 
+//define two numbers
+let number1 = 20;
+let number2 = 30;
+//add two numbers
+let result = number1 + number2;
+console.log(result);
+export {};
---
+++ Q10.ts
@@ -0,0 +1,11 @@
+//task#10
+// this program adds two number and prints the result 
+//define two numbers
+let number1:number=20;
+let number2:number=30;
+//add two numbers
+let result:number=number1 +number2;
+
+console.log(result);
+
+
