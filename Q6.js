7ecdc9124cfadba70a09b89f707e2cb67f31c94d
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-04-27 09:33:35 -0700

    Add files via upload

---
+++ Q6.js
@@ -0,0 +1,6 @@
+//task#6
+let nameWithWhiteSpace = "\t \n imran  hassan\t \n";
+console.log("name With White Space:", nameWithWhiteSpace);
+let strippedName = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+export {};
