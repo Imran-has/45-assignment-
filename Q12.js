74da8c6061d07dd25634ad471ec77f7cc27f66e5
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-03 07:21:14 -0700

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,8 @@
+"use strict";
+//task#12
+let name1 = ["ali", "khan", "asad", "sami",];
+let message = "Do you like the play footbal";
+console.log(name1[0] + " " + message);
+console.log(name1[1] + " " + message);
+console.log(name1[2] + " " + message);
+console.log(name1[3] + " " + message);
---
+++ Q12.ts
@@ -0,0 +1,8 @@
+//task#12
+let name1:string[]=["ali","khan","asad","sami",]
+let message:string="Do you like the play footbal"
+console.log (name1[0]+" "+message)
+console.log (name1[1]+" "+message)
+console.log (name1[2]+" "+message)
+console.log (name1[3]+" "+message)
+
