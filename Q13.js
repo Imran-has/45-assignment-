f83224990d284b7136a467b814cbff00308a9536
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-03 07:25:34 -0700

    Add files via upload

---
+++ Q13.js
@@ -0,0 +1,9 @@
+//task#13
+let transportation = ["honda motorcycle", "mehran car", "honda city"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log(`I would to my a ${transportation[i]}`);
+}
+transportation.forEach(Bus => {
+    console.log(`I would like to my${Bus}`);
+});
+export {};
