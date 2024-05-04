d0a917db000b563dd005b67e388e293104a63e23
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-04 08:46:48 -0700

    Add files via upload

---
+++ Q14.js
@@ -0,0 +1,14 @@
+//tas#14
+let guestarray = ["saad", "sami", "hassan", "khan"];
+guestarray.forEach(guest => {
+    console.log(`Dear ${guest}, you are cordially invited to  dinner bhalee kare aya`);
+});
+//map() method provide us new array []
+//invite guest
+let invitation = guestarray.map(guest => `Dear ${guest}, you are cordially invited to  dinner bhalee kare aya`);
+console.log(invitation);
+//forEach method do not provide new array, its return same result 
+invitation.forEach(invitations => {
+    console.log(invitations);
+});
+export {};
