d78869b698d271b367325565d800cf28adef179c
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-07 07:42:01 -0700

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//taks#20
+let countries = ["Pakistan", "Iran", "Newyork", "Japan", "China",];
+console.log("list of countries:", countries.forEach(country => {
+    console.log(country);
+}));
---
+++ Q20.ts
@@ -0,0 +1,6 @@
+//taks#20
+let countries:string[]=["Pakistan","Iran","Newyork","Japan","China",]
+console.log("list of countries:",countries.forEach(country=>{
+   console.log(country);
+}));
+
